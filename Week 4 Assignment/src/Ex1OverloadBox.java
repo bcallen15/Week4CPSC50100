f9815ce63a477024259854dd65d4f76737566b9e
bcallen15 <67833609+bcallen15@users.noreply.github.com>
2020-07-27 22:29:12 -0500

    added explanation

--- Week 4 Assignment/src/Ex1OverloadBox.java
+++ Week 4 Assignment/src/Ex1OverloadBox.java
@@ -17,7 +17,7 @@
 		
 	public void printBox(char c) {
 		for (int r = 0; r<size; r++) {
-			for (int d = 0; d<size; d++) {
+			for (int d = 0; d<size; d++) { //had to change c to d to avoid double defining the variable
 			System.out.print("c");
 		}
 			System.out.println();
